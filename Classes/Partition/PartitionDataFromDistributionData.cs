67c11ed740e83acb07dd33914a3680a909336ecc
Peter Bauer <bauer@gsg-online.com>
2021-11-30 16:19:17 +0100

    Reworked

--- Classes/Partition/PartitionDataFromDistributionData.cs
+++ Classes/Partition/PartitionDataFromDistributionData.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Masterarbeit.Classes.HospitalData;
 using Masterarbeit.Interfaces.Feature;
 using Masterarbeit.Interfaces.MasterData;
 using Masterarbeit.Interfaces.Partition;
+using Masterarbeit.Interfaces.Service;
 
 namespace Masterarbeit.Classes.Partition
 {
@@ -18,16 +20,17 @@ namespace Masterarbeit.Classes.Partition
             _partitionCount = partitionCount;
         }
 
-        public IEnumerable<IPartition> GlobalPartitions => PartitionData().GlobalPartitions;
         public IEnumerable<IPartition> OpsPartitions => PartitionData().OpsPartitions;
         public IEnumerable<IPartition> DrgPartitions => PartitionData().DrgPartitions;
         public IEnumerable<IPartition> MlgPartitions => PartitionData().MlgPartitions;
         public IEnumerable<IPartition> MdcPartitions => PartitionData().MdcPartitions;
+        public Func<IList<IService>, IList<IDistributionDataService>, int, IEnumerable<IPartition>> PartitionFunction { get; set; }
 
-        public IEnumerable<IFeature> SelectFeaturesFromPartitions(IEnumerable<int> partitionIds, int maxSelected, bool global) =>
-            PartitionData().SelectFeaturesFromPartitions(partitionIds, maxSelected, global);
+        public IEnumerable<IFeature> SelectFeaturesFromPartitions(IEnumerable<int> partitionIds, int maxSelected) =>
+            PartitionData().SelectFeaturesFromPartitions(partitionIds, maxSelected);
 
         private IPartitionData PartitionData() =>
-            _partitionData ??= new PartitionDataFromHospitalData(new HospitalDataFromMasterData(_distributionData), _distributionData, _partitionCount);
+            _partitionData ??=
+                new PartitionDataFromHospitalData(new HospitalDataFromDistributionData(_distributionData), _distributionData, _partitionCount);
     }
 }
